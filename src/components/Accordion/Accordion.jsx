d743a78700e8e9c9a059f2beafb88bf146b04d22
Máté Bartók <matebartok@Mates-Mac-mini.local>
2024-06-05 03:40:45 +0200

    Managing Multi-Component State with the Context API

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,3 +1,41 @@
+import { createContext, useState, useContext } from "react";
+
+const AccordionContext = createContext();
+
+    
+    
+    
+export function useAccordionContext() {
+    const ctx = useContext(AccordionContext)
+
+    if (!ctx) {
+        throw new Error("Accordion-related components must be wrapped by <Accordion>")
+    }
+
+    return ctx;
+}
+
 export default function Accordion({ children, className }) {
-  return <ul className={className}>{children}</ul>;
+    const [openItemId, setOpenId] = useState()
+
+    function openItem(id) {
+        setOpenId(id)
+    }
+
+    function closeItem() {
+        setOpenId(null)
+    }
+
+    const contextValue = {
+        openItemId,
+        openItem,
+        closeItem
+    }  
+
+
+  return (
+    <AccordionContext.Provider value={contextValue}>
+      <ul className={className}>{children}</ul>
+    </AccordionContext.Provider>
+  );
 }
